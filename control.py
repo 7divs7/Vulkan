34029dba7f794f36bd73afb7ca315638ea11ead2
7enTropy7 <menon.uk1998@gmail.com>
2021-03-19 23:37:16 +0530

    edge turns added

--- control.py
+++ control.py
@@ -1,4 +1,3 @@
-from pygame import *
 import RPi.GPIO as GPIO
 from time import sleep as Sleep
 
@@ -11,37 +10,62 @@ GPIO.setup(35,GPIO.OUT)
 GPIO.setup(33,GPIO.OUT)
 GPIO.setup(31,GPIO.OUT)
 
-
 def forward():
+    GPIO.output(37,GPIO.LOW)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(33,GPIO.HIGH)
+    GPIO.output(31,GPIO.LOW)
+    Sleep(0.2)
+
+def edge_forward():
+    GPIO.output(37,GPIO.LOW)
+    GPIO.output(35,GPIO.HIGH)
+    GPIO.output(33,GPIO.HIGH)
+    GPIO.output(31,GPIO.LOW)
+    Sleep(0.535)
+
+def edge_backward():
     GPIO.output(37,GPIO.HIGH)
     GPIO.output(35,GPIO.LOW)
     GPIO.output(33,GPIO.LOW)
     GPIO.output(31,GPIO.HIGH)
-    Sleep(2)
+    Sleep(0.265)
 
 def left():
-    GPIO.output(37,GPIO.HIGH)
+    GPIO.output(37,GPIO.LOW)
     GPIO.output(35,GPIO.LOW)
     GPIO.output(33,GPIO.HIGH)
     GPIO.output(31,GPIO.LOW)
-    Sleep(0.5)
+    Sleep(0.43)
 
 def right():
     GPIO.output(37,GPIO.LOW)
     GPIO.output(35,GPIO.HIGH)
     GPIO.output(33,GPIO.LOW)
-    GPIO.output(31,GPIO.HIGH)
-    Sleep(0.5)
+    GPIO.output(31,GPIO.LOW)
+    Sleep(0.43)
     
 def stop():
     GPIO.output(37,GPIO.LOW)
     GPIO.output(35,GPIO.LOW)
     GPIO.output(33,GPIO.LOW)
     GPIO.output(31,GPIO.LOW)
-    Sleep(5)
+    Sleep(3)
 
-# forward()
+for i in range(10):
+    forward()
+    stop()
+
 right()
+stop()
+
+edge_backward()
+stop()
+
+right()
+stop()
+
+edge_forward()
 stop()
 
 GPIO.cleanup
